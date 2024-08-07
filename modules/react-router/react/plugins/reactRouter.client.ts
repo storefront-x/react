e45fc182f4b16b4179b79df7a639886a3a2ee211
Adam Jedlička <mail@adamjedlicka.cz>
2024-08-07 12:43:36 +0300

    Add support for baseUrl

--- modules/react-router/react/plugins/reactRouter.client.ts
+++ modules/react-router/react/plugins/reactRouter.client.ts
@@ -1,10 +1,13 @@
+import BASE_URL from '#ioc/config/BASE_URL'
 import { createBrowserRouter } from 'react-router-dom'
 import { routes } from '~/.sfx/pages'
 
 export default async (ctx: any) => {
-  const router = createBrowserRouter(routes)
+  const router = createBrowserRouter(routes, {
+    basename: BASE_URL,
+  })
 
-  await router.navigate(window.location.pathname)
+  await router.navigate(window.location.pathname.replace(BASE_URL, '/'))
 
   ctx.router = router
 }
